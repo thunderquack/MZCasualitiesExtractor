787365568137a2d9bfef00a564dd97404718e8e7
Egor Ivanov <egor.i.ivanov@gmail.com>
2023-12-30 20:00:18 +0100

    csv save

--- extract_losses.py
+++ extract_losses.py
@@ -31,12 +31,12 @@ while attempt < max_attempts:
         client = httpx.Client(http2=True)
         response = client.get(mz_page, headers=headers)
         if response.status_code == 200:
-            print('js is found')  
+            print('js is found')
             page_content = response.text
             js_gz_urls = re.findall(url_pattern, page_content)
             url_js = js_gz_urls[0] if js_gz_urls else "URL is not found"
             print(f"Found JS URL: {url_js}")
-            response = client.get(url_js, headers=headers)      
+            response = client.get(url_js, headers=headers)
             js = response.text
             print(js)
             break  # Если успешно, выходим из цикла
@@ -58,25 +58,28 @@ bo_pattern = r'bo\s*=\s*JSON\.parse\(\'({.*?})\'\)'
 # Поиск соответствия в файле
 bo_match = re.search(bo_pattern, js, re.DOTALL)
 # Если найдено соответствие, извлекаем и суммируем все числа
-start_date =  datetime(2022, 2, 24)
+start_date = datetime(2022, 2, 24)
+csv_filename = 'last_data.csv'
+current_date = datetime.now().strftime('%Y-%m-%d')
+current_date_csv = f'{current_date}_cumsum.csv'
 
 if bo_match:
     try:
         # Извлекаем JSON из строки и преобразуем его в объект Python
         bo_data = json.loads(bo_match.group(1))
         rows = len(bo_data)
-        max_len=0
+        max_len = 0
         for key in bo_data:
-            max_len=max(len(bo_data[key]), max_len)
+            max_len = max(len(bo_data[key]), max_len)
         dates = list()
         deltas = list()
         for i in range(max_len):
             date = start_date+timedelta(days=i)
             dates.append(date)
-            s=0
+            s = 0
             for key in bo_data:
                 try:
-                    s+=bo_data[key][i]
+                    s += bo_data[key][i]
                 except:
                     pass
             deltas.append(s)
@@ -86,11 +89,14 @@ if bo_match:
         }
         df = pd.DataFrame(data)
         df['total'] = df['change'].cumsum()
-        print(df.tail())
+        df.to_csv(csv_filename, index=False)
+        df.to_csv(current_date_csv, index=False)
 
     except json.JSONDecodeError as e:
         total_sum_bo = f"Error parsing JSON for 'bo': {e}"
+        print(total_sum_bo)
+        exit(998)
 else:
     total_sum_bo = "No match for variable 'bo' found."
-
-total_sum_bo
+    print(total_sum_bo)
+    exit(998)
