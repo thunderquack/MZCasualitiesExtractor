c530f48461bbe301011a6d1e0d3a7ccc263ac7c9
Egor Ivanov <egor.i.ivanov@gmail.com>
2023-12-30 18:03:47 +0100

    another header

--- extract_losses.py
+++ extract_losses.py
@@ -3,16 +3,14 @@ import re
 
 mz_page = 'https://zona.media/casualties'
 headers = {
-"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20100101 Firefox/121.0",
-"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-"Accept-Language": "en-GB",
-"Accept-Encoding": "gzip",
-"DNT": "1",
-"Connection": "keep-alive",
-"Upgrade-Insecure-Requests": "1",
-"Sec-Fetch-Dest": "document",
-"Sec-Fetch-Mode": "navigate",
-"Sec-Fetch-Site": "cross-site",
+    "User-Agent": "Mozilla/5.0",
+    "Accept": "text/html",
+    "Accept-Encoding": "gzip",
+    "Accept-Language": "en-GB",
+    "Connection": "keep-alive",
+    "DNT": "1",
+    "Upgrade-Insecure-Requests": "1",
+    "Host": "zona.media",
 }
 
 url_pattern = r'https://[a-zA-Z0-9.-]*/infographics/bodycount/[a-zA-Z0-9.-]*\.js\.gz'
@@ -21,7 +19,6 @@ url_pattern = r'https://[a-zA-Z0-9.-]*/infographics/bodycount/[a-zA-Z0-9.-]*\.js
 
 try:
     client = httpx.Client(http2=True)
-
     # Выполняем запрос
     response = client.get(mz_page, headers=headers)
     if response.status_code == 200:
@@ -29,7 +26,10 @@ try:
         page_content = response.text
         js_gz_urls = re.findall(url_pattern, page_content)
         url_js = js_gz_urls[0] if js_gz_urls else "URL is not found"
-        print(url_js)        
+        print('js is found')  
+        response = client.get(url_js, headers=headers)      
+        js = response.text
+        print(js)
     else:
         print(f"Error loading page: {response.status_code}")
 except:
